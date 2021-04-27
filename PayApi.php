d8d3443aac97551f8802c0f6d82a5aa0e8dc6b6a
Mark Page <mark.page@burdenandburden.co.uk>
2021-04-27 12:10:26 +0000

    Pulls HTML from method start() into form_payment.php, renames form.php to firm_signup.php

--- PayApi.php
+++ PayApi.php
@@ -289,6 +289,7 @@ class PayApi {
             $error = 'Email address is not valid';
         }
         else {
+
             // Insert into stripe_payment leaving especially `Paid` and `Created` as null
             // $this->txn_ref = something unique to go in button
             $this->txn_ref = uniqid();
@@ -300,49 +301,9 @@ class PayApi {
               // Verify your integration in this guide by including this parameter
               'metadata' => ['integration_check' => 'accept_a_payment'],
             ]);
-            ?>
 
+            require __DIR__.'/form_payment.php';
 
-            <form id="payment-form">
-            <?php if (STRIPE_DEV_MODE) { ?>
-                <table>
-                <tr><td>Payment succeeds</td><td>4242 4242 4242 4242</td></tr>
-                <tr><td>Payment requires authentication</td><td>4000 0025 0000 3155</td></tr>
-                <tr><td>Payment is declined</td><td>4000 0000 0000 9995</td></tr>
-                </table>
-            <?php } ?>
-
-              <div id="card-element"><!--Stripe.js injects the Card Element--></div>
-              <button id="submit">
-                <div class="spinner hidden" id="spinner"></div>
-                <span id="button-text">Pay now</span>
-              </button>
-              <p id="card-error" role="alert"></p>
-              <p class="result-message hidden">
-                Payment succeeded, see the result in your
-                <a href="" target="_blank">Stripe dashboard.</a> Refresh the page to pay again.
-              </p>
-            </form>
-
-            <script type="text/javascript">
-                var stripe = Stripe('<?php echo STRIPE_PUBLIC_KEY ?>');
-                var clientSecret = "<?php echo $intent->client_secret ?>";
-                var purchase = {
-                  items: [{ id: "xl-tshirt" }]
-                };
-                <?php require "client.js"; ?>
-
-
-                /*var elements = stripe.elements();
-                var style = {
-                  base: {
-                    color: "#32325d",
-                  }
-                };
-                var card = elements.create("card", { style: style });
-                card.mount("#card-element");*/
-            </script>
-            <?php
         }
         if ($error) {
             throw new \Exception ($error);
---
+++ form_payment.php
@@ -0,0 +1,43 @@
+
+            <form id="payment-form">
+            <?php if (STRIPE_DEV_MODE) { ?>
+                <table>
+                <tr><td>Payment succeeds</td><td>4242 4242 4242 4242</td></tr>
+                <tr><td>Payment requires authentication</td><td>4000 0025 0000 3155</td></tr>
+                <tr><td>Payment is declined</td><td>4000 0000 0000 9995</td></tr>
+                </table>
+            <?php } ?>
+
+              <div id="card-element"><!--Stripe.js injects the Card Element--></div>
+              <button id="submit">
+                <div class="spinner hidden" id="spinner"></div>
+                <span id="button-text">Pay now</span>
+              </button>
+              <p id="card-error" role="alert"></p>
+              <p class="result-message hidden">
+                Payment succeeded, see the result in your
+                <a href="" target="_blank">Stripe dashboard.</a> Refresh the page to pay again.
+              </p>
+            </form>
+
+            <script type="text/javascript">
+
+                var stripe = Stripe('<?php echo STRIPE_PUBLIC_KEY ?>');
+                var clientSecret = "<?php echo $intent->client_secret ?>";
+                var purchase = {
+                  items: [{ id: "xl-tshirt" }]
+                };
+
+                <?php require __DIR__.'/client.js'; ?>
+
+                /*var elements = stripe.elements();
+                var style = {
+                  base: {
+                    color: "#32325d",
+                  }
+                };
+                var card = elements.create("card", { style: style });
+                card.mount("#card-element");*/
+
+            </script>
+
